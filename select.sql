fa2d5a7ee36e9e0fdd829bb124379f7dd845c073
Уваров Илья <popodoss@mail.ru>
2022-12-04 05:41:04 +0300

    dorabotka

--- select.sql
+++ select.sql
@@ -24,33 +24,69 @@ group by genrer_id
 order by count(executor_id); 
 
 
-select name, a.album_id from musik m 
+select count(a.album_id)  from musik m 
 join album a on m.album_id = a.album_id 
-where year between 2019 and 2020
-group by name , a.album_id;
+where year   cvbetween 2019 and 2020
+group by  a.album_id;
 
 select album_id  , avg(duration) from musik  
 group by album_id  
 order by avg(duration) desc ;
 
-select name, album_id from album a
-join executor e on e.executor_id = e.executor_id 
-where year !=  2020
-group by  name, album_id  ; 
+select  e.name from executor e
+where e.name not in (
+	select distinct name from executor e2  
+	join album_tupe at2 on e2.executor_id  = at2.executor_id 
+	join album a on a.album_id = at2.album_id 
+	where a.year = 2020
+	)
+order by e.name ;
 
-select  compilation_id , e.name ,executor_id from executor e  
-join compilation c on c.compilation_id = c.compilation_id 
+
+
+select distinct c.name from compilation c 
+join music_collection mc on c.compilation_id = mc.compilation_id 
+join musik m on m.musik_id = mc.musik_id 
+join album a on a.album_id = m.album_id  
+join album_tupe at2 on at2.album_id  = a.album_id 
+join executor e on e.executor_id = at2.executor_id 
 where e.name like '%Горшенёв%'
-group by executor_id ,e.name, c.compilation_id;
+ 	 
+6
+
+select title from album a 
+join album_tupe at2 on at2.album_id = a.album_id 
+join executor e on e.executor_id = at2.executor_id 
+join artist_genres ag on ag.executor_id = e.executor_id 
+join genrer g on g.genrer_id = ag.genrer_id 
+group by title 
+having count(distinct g.genrer_id) > 1
+order by title 
 
 select m.name from musik m 
 left join music_collection mc on m.musik_id = mc.musik_id 
 where duration = null;
 
-select e.name, min(duration) from musik m 
-left join executor e on m.name = e.name
-group by e.name;
 
-select title, name from album a 
+select e.name, m.duration from musik m
+join album a on a.album_id = m.album_id 
+join album_tupe at2 on at2.executor_id = a.album_id 
+join executor e on e.executor_id = at2.executor_id 
+group by e.name, m.duration 
+having m.duration = (select min(duration)from musik m2)
+order by e.name
+
+
+select title from album a 
 join musik m on a.album_id = m.album_id 
-group by title , name;
+where m.album_id in (
+	select album_id from musik m2
+	group by album_id 
+	having count(album_id) =(
+		select count (album_id) from musik m3 
+		group by album_id
+		order by count limit 1
+		)
+	)
+order by title ;
+
